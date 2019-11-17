db5163af303b18c56eee02424c5835650bef2856
Luis Alejandro Avila Guevara <luaavilagu@unal.edu.co>
2019-11-17 00:20:26 -0500

    Actualizacion de repositorios y creación de servicios

---
+++ src/main/java/co/edu/unal/software_engineering/meetu/service/LocationService.java
@@ -0,0 +1,23 @@
+package co.edu.unal.software_engineering.meetu.service;
+
+import co.edu.unal.software_engineering.meetu.model.Location;
+import co.edu.unal.software_engineering.meetu.repository.LocationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    public LocationService(LocationRepository locationRepository){
+        this.locationRepository = locationRepository;
+    }
+
+    public Location findById(Integer id ){
+        return locationRepository.findById( id ).orElse( null );
+    }
+
+    public Location findByPlanId( String planId ){
+        return locationRepository.findByPlanId(  planId );
+    }
+}

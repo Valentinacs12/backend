2779b05e284da05b0436ea2030fee97b668b2811
Tomas Velasquez <mtvelasquezg@unal.edu.co>
2019-11-17 00:16:22 -0500

    se crea la logica que permite añadir diferentes opciones al plan

--- src/main/java/co/edu/unal/software_engineering/meetu/repository/OptionRepository.java
+++ src/main/java/co/edu/unal/software_engineering/meetu/repository/OptionRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Integer>{
 
+    Option findByPlanId(String planId);
 }

ebeb1355040f1f1bf233c334a3668e73df8bd84a
Tomas Velasquez <mtvelasquezg@unal.edu.co>
2019-11-16 23:47:31 -0500

    se crean el repositorios faltantes

--- src/main/java/co/edu/unal/software_engineering/meetu/repository/OptionRepository.java
+++ src/main/java/co/edu/unal/software_engineering/meetu/repository/OptionRepository.java
@@ -1,11 +1,11 @@
 package co.edu.unal.software_engineering.meetu.repository;
 
-import co.edu.unal.software_engineering.meetu.model.Budget;
+import co.edu.unal.software_engineering.meetu.model.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface BudgetRepository extends JpaRepository<Budget, Integer>{
+public interface OptionRepository extends JpaRepository<Option, Integer>{
 
 }

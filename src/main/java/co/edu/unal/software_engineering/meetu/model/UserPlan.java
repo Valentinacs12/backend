6590320ebbd4c6162ca80f9e07b5a1c6d9bc54aa
Luis Alejandro Avila Guevara <luaavilagu@unal.edu.co>
2019-11-16 23:26:28 -0500

    Se agrega la clase UserPlan.java

---
+++ src/main/java/co/edu/unal/software_engineering/meetu/model/UserPlan.java
@@ -0,0 +1,123 @@
+package co.edu.unal.software_engineering.meetu.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Private package class for the relation with M...M table
+ */
+
+@Entity
+@Table( name = "user_plan", schema = "public" )
+class UserPlan implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Attributes
+     */
+
+    @EmbeddedId
+    private UserPlanPK userPlanPK;
+
+    /**
+     * Constructors
+     */
+
+    public UserPlan( ){ }
+
+    /**
+     * Getters and Setters
+     */
+
+    User getUser( ){
+        return userPlanPK.getUser( );
+    }
+
+    void setUser( User user ){
+        userPlanPK.setUser( user );
+    }
+
+    Plan getPlan( ){
+        return userPlanPK.getPlan( );
+    }
+
+    void setPlan( Plan plan ){
+        userPlanPK.setPlan( plan );
+    }
+
+    /**
+     * Methods
+     */
+
+    @Override
+    public boolean equals( Object object ){
+        if( !(object instanceof UserPlan) ) return false;
+        UserPlan userPlan = (UserPlan) object;
+        return getUser( ).getId( ).equals( userPlan.getUser( ).getId( ) ) &&
+                getPlan( ).getIdPlan( ).equals( userPlan.getPlan( ).getIdPlan( ) );
+    }
+
+    @Override
+    public int hashCode( ){
+        final int prime = 31;
+        int hash = 17;
+        hash = hash * prime + getUser( ).hashCode( );
+        hash = hash * prime + getPlan( ).hashCode( );
+        return hash;
+    }
+
+    /**
+     * Private class for primary key
+     */
+
+    @Embeddable
+    private static class UserPlanPK implements Serializable{
+
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * Attributes
+         */
+
+        @ManyToOne
+        @JoinColumn( name = "user_id", insertable = false, updatable = false )
+        private User user;
+
+        @ManyToOne
+        @JoinColumn( name = "idPlan", insertable = false, updatable = false )
+        private Plan plan;
+
+        /**
+         * Constructor
+         */
+
+        public UserPlanPK( ){ }
+
+        public UserPlanPK( User user, Plan plan ){
+            this.user = user;
+            this.plan = plan;
+        }
+
+        /**
+         * Getters and Setters
+         */
+
+        public User getUser( ){
+            return user;
+        }
+
+        public void setUser( User user ){
+            this.user = user;
+        }
+
+        public Plan getPlan( ){
+            return plan;
+        }
+
+        public void setPlan( Plan plan ){
+            this.plan = plan;
+        }
+    }
+
+}

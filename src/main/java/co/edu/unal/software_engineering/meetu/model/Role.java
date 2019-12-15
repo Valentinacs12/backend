b277b51c07a22c7dca7f401e0e205f0f1577f11f
Luis Alejandro Avila Guevara <luaavilagu@unal.edu.co>
2019-12-15 18:21:13 -0500

    Pruebas de Role. Roles usuario y administrador.

--- src/main/java/co/edu/unal/software_engineering/meetu/model/Role.java
+++ src/main/java/co/edu/unal/software_engineering/meetu/model/Role.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,6 +16,15 @@ import java.util.List;
 public class Role implements Serializable{
 
     private static final long serialVersionUID = 1L;
+
+    private final static int ROLE_USER_ID = 1;
+    private final static String ROLE_USER_NAME = "User";
+    private final static int ROLE_ADMIN_ID = 2;
+    private final static String ROLE_ADMIN_NAME = "Administrator";
+
+    private final static Role USER = new Role( ROLE_USER_ID, ROLE_USER_NAME );
+    private final static Role ADMIN = new Role( ROLE_ADMIN_ID, ROLE_ADMIN_NAME );
+
 
     /**
      * Attributes
@@ -37,6 +47,12 @@ public class Role implements Serializable{
      */
 
     public Role( ){ }
+
+    private Role (Integer id, String roleName){
+        this.id = id;
+        this.roleName = roleName;
+        this.users = new ArrayList<>();
+    }
 
     /**
      * Getters and Setters
@@ -69,6 +85,15 @@ public class Role implements Serializable{
     /**
      * Methods
      */
+
+    public static Role getUser( ){
+        return USER;
+    }
+
+    public static Role getAdmin( ){
+        return ADMIN;
+    }
+
 
     @Override
     public boolean equals( Object object ){
---
+++ src/test/java/co/edu/unal/software_engineering/meetu/RoleServiceTest.java
@@ -0,0 +1,57 @@
+package co.edu.unal.software_engineering.meetu;
+
+
+import co.edu.unal.software_engineering.meetu.model.Role;
+import co.edu.unal.software_engineering.meetu.repository.RoleRepository;
+import co.edu.unal.software_engineering.meetu.service.RoleService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@RunWith( SpringRunner.class )
+@DataJpaTest
+@AutoConfigureTestDatabase( replace = NONE )
+public class RoleServiceTest{
+
+    @TestConfiguration
+    static class RoleServiceTestImplTestContextConfiguration{
+        @Autowired
+        private RoleRepository roleRepository;
+
+        @Bean
+        public RoleService roleService( ) {
+            return new RoleService( roleRepository );
+        }
+    }
+
+    @Autowired
+    private RoleService roleService;
+
+
+    public RoleServiceTest( ){ }
+
+
+    @Test
+    public void findByIdTest( ){
+        Role user = roleService.findById( Role.getUser( ).getId( ) );
+        Role admin = roleService.findById( Role.getAdmin( ).getId( ) );
+        assertEquals( user, Role.getUser( ) );
+        assertEquals( admin, Role.getAdmin( ) );
+
+
+
+
+        assertNotEquals( user, Role.getAdmin( ) );
+        assertNotEquals( admin, Role.getUser( ) );
+    }
+
+}
